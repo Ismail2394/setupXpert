c4a1836132320658a459ebf2b59a84c42a30a116
stefan5269 <stk5269@thi.de>
2023-01-10 18:55:46 +0100

    Diverse Beans/servlets und linkanpassungen

--- ProjectRed/src/main/java/beans/kontakt.java
+++ ProjectRed/src/main/java/beans/kontakt.java
@@ -6,14 +6,12 @@ import java.io.Serializable;
 public class kontakt implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private int kontakt_id;
-	private String productType1;
-	private String productType2;
-	private String productType3;
 	private String eMail;
 	private String geschlecht;
-	private String firstName;
-	private String lastName;
+	private String vorName;
+	private String nachName;
 	private String custmessage;
+	private int bestID;
 
 	public int getKontakt_id() {
 		return kontakt_id;
@@ -39,22 +37,6 @@ public class kontakt implements Serializable {
 		this.geschlecht = geschlecht;
 	}
 
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
 	public String getCustmessage() {
 		return custmessage;
 	}
@@ -63,29 +45,27 @@ public class kontakt implements Serializable {
 		this.custmessage = custmessage;
 	}
 
-	public String getProductType1() {
-		return productType1;
+	public int getBestID() {
+		return bestID;
 	}
 
-	public void setProductType1(String productType1) {
-		this.productType1 = productType1;
+	public void setBestID(int bestID) {
+		this.bestID = bestID;
 	}
 
-	public String getProductType3() {
-		return productType3;
+	public String getVorName() {
+		return vorName;
 	}
 
-	public void setProductType3(String productType3) {
-		this.productType3 = productType3;
+	public void setVorName(String vorName) {
+		this.vorName = vorName;
 	}
 
-	public String getProductType2() {
-		return productType2;
+	public String getNachName() {
+		return nachName;
 	}
 
-	public void setProductType2(String productType2) {
-		this.productType2 = productType2;
+	public void setNachName(String nachName) {
+		this.nachName = nachName;
 	}
-
-	
 }
