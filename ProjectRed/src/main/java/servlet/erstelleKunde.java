a6e1c9892fdf3fddb6127466b9f0b0157fec8435
Ismail2394 <isc0747@thi.de>
2023-03-15 21:31:39 +0100

    -KP

--- ProjectRed/src/main/java/servlet/erstelleKunde.java
+++ ProjectRed/src/main/java/servlet/erstelleKunde.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/erstelleKunde")
+@WebServlet("/ersteleKunde")
 public class erstelleKunde extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
